ea1ad71983b063033341e056690de581a0a0da6c
Daniel Vera <danieelveera@outlook.com>
2023-08-31 11:05:36 -0400

    recent posts addded

--- app/post/[slug]/page.tsx
+++ app/post/[slug]/page.tsx
@@ -2,6 +2,7 @@ import { Post } from "@/app/lib/interface";
 import {client} from "@/app/lib/sanity"
 import { urlFor } from "@/app/lib/sanityImageUrl";
 import { PortableText } from "@portabletext/react";
+import Link from "next/link";
 
 async function getData(slug: string){
     const query = `*[_type=="post" && slug.current == "${slug}"][0]`
@@ -11,8 +12,19 @@ async function getData(slug: string){
     return data;
 }
 
+async function getAllPosts() {
+    const query = `*[_type == "post"][0...2]`
+  
+    const data = await client.fetch(query);
+  
+    return data;
+  }
+
 export default async function SlugPage({params}: {params: {slug: string}}){
     const data = (await getData(params.slug)) as Post;
+
+    const posts = (await getAllPosts()) as Post[];
+
 
     const PortableTextComp = {
         types: {
@@ -38,11 +50,39 @@ export default async function SlugPage({params}: {params: {slug: string}}){
 
             <div className="divide-y divide-gray-200 py-7 max-w-4xl mx-auto">
                 <div>
-                    <div className="m-8 space-y-5">
+                    <div className="m-8 space-y-5 text-ellipsis">
                         
                         <PortableText value={data.content} components={PortableTextComp}/>
                     </div>
                 </div>
+            </div>
+
+            <hr className="mx-10"/>
+            <h1 className="text-center my-10 text-2xl font-extrabold text-green">Recent Posts</h1>
+            <div className="flex mx-4 space-x-8 mb-12 md:flex-col md:justify-center md:items-center md:max-w-lg md:mx-auto">
+            {posts.map((post) => (
+          <li
+            key={post._id}
+            className="my-4 mx-auto w-5/6 text-center border-black border-2 rounded-lg"
+          >
+            <article className="space-y-8 p-4">
+              <Link href={`/post/${post.slug.current}`} prefetch className="">
+                <div>
+                  <h3 className="text-xl font-semibold leading-normal">
+                    {post.title}
+                  </h3>
+                </div>
+              </Link>
+
+              
+
+
+              <button className="btn-1">
+                <a href={`/post/${post.slug.current}`}>Read post</a>
+              </button>
+            </article>
+          </li>
+        ))}
             </div>
         </div>
     )
